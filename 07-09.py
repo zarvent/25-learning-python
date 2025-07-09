50a1b7b9ec880f52c7ef55141dcc4b071f79e221
zarvent <zarventt@gmail.com>
2025-07-09 16:53:44 -0400

    Refactor bool_to_word function and update comments for clarity

--- 07-09.py
+++ 07-09.py
@@ -1,3 +1,12 @@
+# SESION DE APRENDIZAJE 2025-07-09
+# En Python, 'def' se utiliza para definir una función.
+# Implementare la funcion bool_to_word
+def bool_to_word(boolean):
+    # analogia [imaginemos un interruptor de luz]
+    # tenemos que expresar que cuando el interruptor estre prendido (osea *postivo*) diga *yes*
+    # si el interrupto esta apagado (osea *false*) la funcion diga *no*
+    if boolean:
+        return "Yes"
+    else:
+        return "No"
 
-""sdasd"""
-def bool_to_word(booleano)
