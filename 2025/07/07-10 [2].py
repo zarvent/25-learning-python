3528cd664cc4c7cc9fdc403c6acd1169e1396456
zarvent <zarventt@gmail.com>
2025-07-10 11:38:07 -0400

    Add new functions 'maps' and 'opposite' with accompanying comments and examples

---
+++ 2025/07/07-10 [2].py
@@ -0,0 +1,16 @@
+# Sesion 2025-07-10
+'''
+Aprendido hoy:
+# 'pass' es como decirle a python "no hagas nada"
+# 'append' sirve para agregar un elemento al final de una lista existente en Python. Por ejemplo: mi_lista.append(4) agrega el número 4 al final de la lista mi_lista.
+'''
+
+# Creamos la funcion 'maps'
+def maps(a):
+    # Creamos una lista vacia
+    lista_resultado = []
+    # Creamos bucle for que hace que recorra cada numero de la lista
+    for number in a:
+        # Ahora necesitamos "guardar" el nuevo valor * 2
+        lista_resultado.append(number * 2)
+    return lista_resultado
---
+++ 2025/07/07-10.py
@@ -0,0 +1,14 @@
+# Sesion 2025-07-10
+#Creamos la funcion opposite
+def opposite(number):  # Definimos la función que devuelve el opuesto del número
+    return -number
+
+'''
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+'''
