13c62e1c2bded361fcd74de92bf5b38b83dd6c4b
zarvent <zarventt@gmail.com>
2025-07-12 21:45:33 -0400

    Add login testing function and user prompts in Spanish

---
+++ 2025/07/07-12 [2].py
@@ -0,0 +1,8 @@
+def probando_login():
+    pass
+
+print("Bienvenido a este test 💌 \n")
+usuario = input("Ingrese su usuario: \n")
+print('Usuario ingresado: 💌 Gracias por registrarte \n', "😎 " + usuario)
+password = input("Ahora ingresa tu password: \n")
+print("Password ingresado: ", "💌 Gracias " + usuario)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Empezando a aprender python hoy 2025-07-09
+
