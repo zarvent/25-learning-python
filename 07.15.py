93a3658e1a41799f4d94c2a8621bd7752e1e6fb6
zarvent <zarventt@gmail.com>
2025-07-16 12:03:00 -0400

    Remove outdated files and add initial content for Python learning exercises

--- 07.15.py
+++
@@ -1,1 +0,0 @@
-# Hoy 2025-07-15 a las 23:50 no complete ningun ejercicio, mañana lo hare
---
+++ 2025/07/07-15[2].py
@@ -0,0 +1,22 @@
+'''
+Estabas acampando con tus amigos lejos de casa, pero cuando llega el momento de regresar, te das cuenta de que tu combustible se está acabando y la gasolinera más cercana está a 80 kilómetros de distancia (50 millas). Sabes que, en promedio, tu coche recorre unos 40 kilómetros por galón (25 millas por galón). Te quedan 3,2 litros (2 galones).
+
+Considerando estos factores, escribe una función que te diga si es posible llegar a la gasolinera o no.
+
+La función debe devolver True si es posible y False si no lo es.
+Happy Coding! ;)
+
+Traducciones de las variable
+distance_to_pump --> Distancia a la gasolinera
+mpg --> Millas por galon
+fuel_left --> Combustible Restante
+'''
+
+
+def zerofuel(distance_to_pump, mpg, fuel_left)
+ #80 kilometros de distancia
+ # 40 kilometros por galon
+# quedan x litro
+    fuel_left * mpg = float(car_tour)
+    if
+
